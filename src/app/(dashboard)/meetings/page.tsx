25236742bf96d085ac3688ee566145514ae84091
ab9898998989898 <abdullahnadeem2580@gmail.com>
2026-01-21 22:50:34 +0500

    17: meetings form

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,25 +1,41 @@
+import { auth } from "@/lib/auth";
+import { ListHeader } from "@/modules/meetings/ui/component/list-header";
 import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const Page = () => {
+const Page = async () => {
 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
         trpc.meeting.getMany.queryOptions({})
     );
 
+    const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in")
+  }
+
+
 
     return (
+       <>
+       <ListHeader />
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<MeetingsViewLoading />}>
-                <ErrorBoundary fallback={<MeetingsViewError />}>
-                    <MeetingsView />
-                </ErrorBoundary>
-            </Suspense>
+         <Suspense fallback={<MeetingsViewLoading />}>
+            <ErrorBoundary fallback={<MeetingsViewError />}>
+                <MeetingsView />
+            </ErrorBoundary>
+         </Suspense>
         </HydrationBoundary>
+       </>
     )
 }
 
---
+++ src/components/command-select.tsx
@@ -0,0 +1,77 @@
+import { useState, ReactNode } from "react";
+import { ChevronsUpDownIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import {
+    CommandEmpty,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandResponsiveDialog
+} from "./ui/command";
+
+interface Props {
+    options: Array<{
+        id: string;
+        value: string;
+        children: ReactNode;
+    }>;
+    onSelect: (value: string) => void;
+    onSearch?: (value: string) => void;
+    value: string;
+    placeholder?: string;
+    className?: string;
+};
+
+export const CommandSelect = ({
+    options,
+    onSelect,
+    onSearch,
+    value,
+    placeholder,
+    className
+}: Props) => {
+    const [open, setOpen] = useState(false);
+    const selectedOptions = options.find((option) => option.value === value);
+
+    return(
+        <>
+            <Button
+                type="button"
+                variant="outline"
+                className={cn(
+                    "h-9 justify-between font-normal px-2", 
+                    !selectedOptions && "text-muted-foreground",
+                    className
+                )}
+                onClick={() => setOpen(true)}
+            >
+                <div>
+                    {selectedOptions?.children ?? placeholder}
+                </div>
+                <ChevronsUpDownIcon />
+            </Button>
+            <CommandResponsiveDialog
+            shouldFilter={!onSearch}
+                open={open}
+                onOpenChange={setOpen}
+            >
+                <CommandInput placeholder="Search..." onValueChange={onSearch} />
+                <CommandList>
+                    <CommandEmpty>
+                        <span className="text-muted-foreground text-sm">No results.</span>
+                    </CommandEmpty>
+                    {options.map((option)=>(
+                        <CommandItem 
+                            key={option.id}
+                            onSelect={()=>{onSelect(option.value); setOpen(false)}}
+                        >
+                            {option.children}
+                        </CommandItem>
+                    ))}
+                </CommandList>
+            </CommandResponsiveDialog>
+        </>
+    )
+};
